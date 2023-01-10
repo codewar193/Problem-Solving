018fe3efa09b5df2578dd01cb4a90fa4daf14517
S. Manna <90498811+snehamanna559@users.noreply.github.com>
2023-01-10 21:35:07 +0530

    Update q8_IsomorphicStrings.cpp

--- q8_IsomorphicStrings.cpp
+++ q8_IsomorphicStrings.cpp
@@ -6,19 +6,16 @@ class Solution {
 public:
     bool isSubsequence(string s, string t) {
         
-        unordered_map<char, vector<int>> m;
-        for(int i=0;i<t.size();++i)
-            m[t[i]].push_back(i);       //Push index of each character in t in ASC order
-        
-        int low = -1;   //Index to be found must be greater than low index
-        for(int i=0;s[i]!='\0';++i)
+        if(s=="")return true;
+
+        int j=0;
+        for(int i =0;i<t.size();i++)
         {
-            int pos = upper_bound(m[s[i]].begin(),m[s[i]].end(),low)-m[s[i]].begin();
-            if(pos==m[s[i]].size())
-                return false;
-            low = m[s[i]][pos];
+           if(t[i]==s[j])j++;
+          
+           if(j==s.size())return true;
         }
-        return true;
+        return false;
     }
 };
 */
