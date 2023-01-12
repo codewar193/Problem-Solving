2e8241a94f88617e38052fbc6b4680bbed41c5ea
S. Manna <90498811+snehamanna559@users.noreply.github.com>
2023-01-12 23:23:37 +0530

    Reverse Linked List.

---
+++ q11_ReverseLinkedList.cpp
@@ -0,0 +1,32 @@
+
+/*
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+*/
+
+/*
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *prev,*temp,*curr;
+
+        curr =head;
+        prev =NULL;
+
+        while(curr){
+            temp =curr->next;
+            curr->next = prev;
+            prev=curr;
+            curr=temp;
+
+        }
+        return prev;
+
+    }
+};
+
+*/
