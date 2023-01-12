75fffd07df008a3f417e6bbed41bd27e32ab0855
S. Manna <90498811+snehamanna559@users.noreply.github.com>
2023-01-12 23:20:02 +0530

    Middle of the LinkedList

---
+++ q10_MiddleoftheLinkedList.cpp
@@ -0,0 +1,42 @@
+
+
+
+/*
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+*/
+
+
+
+
+
+
+/*class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* temp=head;
+        int count=0,i=0;
+
+        while(temp){
+        temp=temp->next;
+        count++;
+        }
+        temp =head;
+         int n = count/2;
+         while(i<n){
+             temp=temp->next;
+             i++;
+         }
+         return temp;
+    }
+};*/
