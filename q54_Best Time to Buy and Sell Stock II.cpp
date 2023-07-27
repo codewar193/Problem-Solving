beef6c9f5dc1bdce29318b4c8616bff9ae2d25f4
S. Manna <90498811+codewar193@users.noreply.github.com>
2023-07-27 19:35:12 +0530

    leetcode #medium

--- q54_Best Time to Buy and Sell Stock II.cpp
+++ q54_Best Time to Buy and Sell Stock II.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
 
-       int profit =0;
+     int profit =0;
 
        for(int i =1;i<prices.size();i++){
            if(prices[i]>prices[i-1]){
