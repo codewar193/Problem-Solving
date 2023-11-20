a3eb052e09763698a06fed22bf5ef21dbeedbd32
S. Manna <90498811+codewar193@users.noreply.github.com>
2023-11-20 11:51:12 +0530

    Update 8_Contains Duplicate II.py

--- python/8_Contains Duplicate II.py
+++ python/8_Contains Duplicate II.py
@@ -1,3 +1,20 @@
+# class Solution(object):
+#     def containsNearbyDuplicate(self, nums, k):
+#         """
+#         :type nums: List[int]
+#         :type k: int
+#         :rtype: bool
+#         """
+#         n = len(nums) 
+#         d= {}
+#         for i,n in enumerate(nums):
+#             if n in d and abs(i-d[n])<=k:
+#                 return True
+#             else:
+#                 d[n]=i    
+
+#         return False             
+        
 class Solution(object):
     def containsNearbyDuplicate(self, nums, k):
         """
@@ -5,13 +22,14 @@ class Solution(object):
         :type k: int
         :rtype: bool
         """
-        n = len(nums) 
-        d= {}
-        for i,n in enumerate(nums):
-            if n in d and abs(i-d[n])<=k:
-                return True
-            else:
-                d[n]=i    
+     
+        E= dict()
+        for i in range (len(nums)):
+            n = nums[i]
+            if n in E:
+                if abs(E[n] - i) <= k:
+                    return True
+            E[n] = i
 
         return False             
         
