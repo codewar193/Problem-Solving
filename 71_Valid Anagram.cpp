06c1be049d424c13ec13b22f7c975507d67cb8eb
S. Manna <90498811+codewar193@users.noreply.github.com>
2023-11-22 11:34:49 +0530

    Create 71_Valid Anagram.cpp

---
+++ 71_Valid Anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+        unordered_map<char,int> mp;
+        unordered_map<char,int> mp1;
+        for(int i =0;i<s.length();i++){
+          mp[s[i]]++;
+        }
+        for(int i =0;i<t.length();i++){
+          mp1[t[i]]++;
+        }
+
+        return mp==mp1;
+    }
+};
