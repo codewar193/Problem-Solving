cbbed859340fbebc238512ac8187aac557bfe0d2
S. Manna <90498811+codewar193@users.noreply.github.com>
2023-11-24 23:56:59 +0530

    Create 11_Rhombus Star.py

---
+++ python/11_Rhombus Star.py
@@ -0,0 +1,11 @@
+for i in range(4):
+    j = 0
+    k = 0
+    while k <= i:
+        print(" ", end="")
+        k += 1
+    while j < 4:
+        print("*", end="")
+        j += 1
+    print()
+# The time complexity of the provided code is O(n^2)
