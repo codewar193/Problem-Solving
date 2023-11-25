dbbd2825c78b9e6c03f40fff6602c42bd29e38ee
S. Manna <90498811+codewar193@users.noreply.github.com>
2023-11-25 15:11:04 +0530

    Create 11_Mirrored Rhombus Star.py

---
+++ python/11_Mirrored Rhombus Star.py
@@ -0,0 +1,13 @@
+for i in range(4,0,-1):
+    j =i
+    while j>0:
+        print(" ",end="")
+        j-=1
+    for k in range(4):
+        print("*",end="")
+    print()    
+
+ #    ****
+ #   ****
+ #  ****
+ # ****
